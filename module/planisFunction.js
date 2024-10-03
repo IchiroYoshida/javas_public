9cbd6fe5e80d277719f8fe68647e560bcf7c2c69
ichiroyoshida <ichiro1@msb.biglobe.ne.jp>
2024-10-03 16:10:51 +0900

    Umi241003

--- module/planisFunction.js
+++ module/planisFunction.js
@@ -1,4 +1,5 @@
 //planisFunction.js
+//Planisphere: Azimuthal equidistant projection
 function drawPolar(az, alt){
     let Az = az * (PI/180);
     let Alt = alt * (PI/180);
@@ -8,5 +9,15 @@ function drawPolar(az, alt){
     let y =  -r * Math.cos(Az);
     let XX = 2*x/PI*drawRadius;
     let YY = 2*y/PI*drawRadius;
+    return{x:XX, y:YY};
+}
+
+//360VR Equirectangular projection
+function Equirect(az, alt){
+    let Az = az  + 180;
+    let Alt = alt +90;
+
+    let XX = 2*Az/PI*drawRadius;
+    let YY = 2*Alt/PI*drawRadius;
     return{x:XX, y:YY};
 }
